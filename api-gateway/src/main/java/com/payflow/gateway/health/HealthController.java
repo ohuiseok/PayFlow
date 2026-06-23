9dc68f98bde9a71c42517bfa2aadd45fb1a67e5d
uiseok <dkdlelxoa9@gmail.com>
2026-06-23 21:09:41 +0900

    feat: cors

--- api-gateway/src/main/java/com/payflow/gateway/health/HealthController.java
+++ api-gateway/src/main/java/com/payflow/gateway/health/HealthController.java
@@ -1,11 +1,13 @@
 package com.payflow.gateway.health;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
 import java.util.Map;
 
+@CrossOrigin(origins = "*")
 @RestController
 public class HealthController {
 
